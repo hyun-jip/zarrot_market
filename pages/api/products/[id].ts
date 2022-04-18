ebd2681eb0c70425f756b78cf37794e2bee209ee
hyun-jip <frank910@naver.com>
2022-04-18 23:38:18 +0900

    11.4 Product Detail

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -1,21 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
-
 import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
   });
-  res.json({
-    ok: true,
-    profile,
-  });
+  console.log(product);
+  res.json({ ok: true, product });
 }
+
 export default withApiSession(
   withHandler({
     methods: ["GET"],
